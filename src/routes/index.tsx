d13afed9c9f413918d961d3e15165b1cb718c021
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-29 00:42:07 +0900

    ⛏   not foundになるのでpageの編集画面に出てくるコードを使用

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,45 +1,40 @@
-import { component$ } from '@builder.io/qwik'
-import { routeLoader$ } from '@builder.io/qwik-city'
+import { component$, Resource, useResource$ } from '@builder.io/qwik'
+import { useLocation } from '@builder.io/qwik-city'
 import {
-  getBuilderSearchParams,
   getContent,
   RenderContent,
+  getBuilderSearchParams,
 } from '@builder.io/sdk-qwik'
 
 export const BUILDER_PUBLIC_API_KEY = import.meta.env
   .VITE_BUILDER_PUBLIC_API_KEY
 export const BUILDER_MODEL = 'page'
 
-// Qwik Cityの `useBuilderContent` を使って、Builderからコンテンツを取得します。
-// `routeLoader$()` は非同期関数を受け取り、 `getContent()` で Builder からコンテンツを取得します。
-
-export const useBuilderContent = routeLoader$(async ({ url, error }) => {
-  const builderContent = await getContent({
-    model: BUILDER_MODEL,
-    apiKey: BUILDER_PUBLIC_API_KEY,
-    options: getBuilderSearchParams(url.searchParams),
-    userAttributes: {
-      urlPath: url.pathname,
-    },
-  })
-  if (!builderContent) {
-    throw error(404, 'File Not Found')
-  }
-  // Builderから取得したコンテンツを返す
-  return builderContent
-})
-
 export default component$(() => {
-  // useBuilderContent() を呼び出し、返された builderContent と同じ内容を設定する。
-  const content = useBuilderContent()
-  // RenderContent は `content` を使用して、与えられたモデル（ここではページ）のコンテンツを
-  // レンダリングします（API キーで指定します）。
+  const location = useLocation()
+
+  const builderContentRsrc = useResource$<any>(() => {
+    return getContent({
+      model: BUILDER_MODEL,
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      options: getBuilderSearchParams(location.query),
+      userAttributes: {
+        urlPath: location.pathname || '/',
+      },
+    })
+  })
 
   return (
-    <RenderContent
-      model={BUILDER_MODEL}
-      content={content.value}
-      apiKey={BUILDER_PUBLIC_API_KEY}
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) => (
+        <RenderContent
+          model={BUILDER_MODEL}
+          content={content}
+          apiKey={BUILDER_PUBLIC_API_KEY}
+        />
+      )}
     />
   )
 })
