cbed39fff97679f4e5d34e1d5689fec2ad61c2c9
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-28 23:00:30 +0900

    ⛏   indexに追加
    https://www.builder.io/c/docs/quickstart#step-2-add-a-builder-component-to-your-app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,171 +1,47 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik'
+import { routeLoader$ } from '@builder.io/qwik-city'
+import {
+  getBuilderSearchParams,
+  getContent,
+  RenderContent,
+} from '@builder.io/sdk-qwik'
+
+export const BUILDER_PUBLIC_API_KEY = process.env.BUILDER_PUBLIC_API_KEY
+export const BUILDER_MODEL = 'page'
+
+// Use Qwik City's `useBuilderContent` to get your content from Builder.
+// `routeLoader$()` takes an async function to fetch content
+// from Builder with `getContent()`.
+export const useBuilderContent = routeLoader$(async ({ url, error }) => {
+  const builderContent = await getContent({
+    model: BUILDER_MODEL,
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: {
+      urlPath: url.pathname,
+    },
+  })
+  // If there's no content, throw a 404.
+  // You can use your own 404 component here
+  if (!builderContent) {
+    throw error(404, 'File Not Found')
+  }
+  // return content fetched from Builder
+  return builderContent
+})
 
 export default component$(() => {
+  // call useBuilderContent() and set content equal to
+  // returned builderContent
+  const content = useBuilderContent()
+  // RenderContent uses `content` to
+  // render the content of the given model, here a page,
+  // of your space (specified by the API Key)
   return (
-    <div>
-      <h1>
-        Welcome to Qwik <span class='lightning'>⚡️</span>
-      </h1>
-
-      <ul>
-        <li>
-          Check out the <code>src/routes</code> directory to get started.
-        </li>
-        <li>
-          Add integrations with <code>npm run qwik add</code>.
-        </li>
-        <li>
-          More info about development in <code>README.md</code>
-        </li>
-      </ul>
-
-      <h2>Commands</h2>
-
-      <table class='commands'>
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class='commands'>
-        <tr>
-          <td>
-            <code>npm run qwik add azure-swa</code>
-          </td>
-          <td>
-            <a
-              href='https://learn.microsoft.com/azure/static-web-apps/overview'
-              target='_blank'
-            >
-              Azure Static Web Apps
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href='https://developers.cloudflare.com/pages' target='_blank'>
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href='https://expressjs.com/' target='_blank'>
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href='https://docs.netlify.com/' target='_blank'>
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static</code>
-          </td>
-          <td>
-            <a
-              href='https://qwik.builder.io/qwikcity/static-site-generation/overview/'
-              target='_blank'
-            >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add vercel-edge</code>
-          </td>
-          <td>
-            <a
-              href='https://vercel.com/features/edge-functions'
-              target='_blank'
-            >
-              Vercel Edge Functions
-            </a>
-          </td>
-        </tr>
-      </table>
-
-      <h2>Community</h2>
-
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href='https://qwik.builder.io/chat' target='_blank'>
-            Chat on discord!
-          </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href='https://twitter.com/QwikDev' target='_blank'>
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href='https://github.com/BuilderIO/qwik' target='_blank'>
-            GitHub
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href='https://qwik.builder.io/media/' target='_blank'>
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
-      <Link class='mindblow' href='/flower/'>
-        Blow my mind 🤯
-      </Link>
-    </div>
-  );
-});
-
-export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
-};
+    <RenderContent
+      model={BUILDER_MODEL}
+      content={content.value}
+      apiKey={BUILDER_PUBLIC_API_KEY}
+    />
+  )
+})
